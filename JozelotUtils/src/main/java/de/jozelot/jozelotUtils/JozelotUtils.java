ebc8c35b0c3a53739aae6ee0af4062949a83fb86
jozelot_ <jozelot77@gmail.com>
2026-01-25 17:48:13 +0100

    Feat: JozelotUtils Backend Plugin (#57)

---
+++ JozelotUtils/src/main/java/de/jozelot/jozelotUtils/JozelotUtils.java
@@ -0,0 +1,17 @@
+package de.jozelot.jozelotUtils;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class JozelotUtils extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
