cdb7ad1e403911338ea1ee7566166dbecbfcc9bb
jozelot_ <jozelot77@gmail.com>
2026-02-24 23:39:03 +0100

    fix: inventory update task now cancels when inventory gets closed

--- JozelotLobby/src/main/java/de/jozelot/jozelotLobby/ui/items/ClickHandler.java
+++ JozelotLobby/src/main/java/de/jozelot/jozelotLobby/ui/items/ClickHandler.java
@@ -8,6 +8,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.inventory.InventoryCloseEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
@@ -95,5 +96,17 @@ public class ClickHandler implements Listener {
         }
 
         lobbyPlayer.playSound("pling");
+    }
+
+    @EventHandler
+    public void onInventoryClose(InventoryCloseEvent event) {
+        Inventory inventory = event.getInventory();
+        if (!(event.getPlayer() instanceof Player player)) {
+            return;
+        }
+
+        if (inventory.getHolder() instanceof NavigatorMenu menu) {
+            menu.stopUpdateTask();
+        }
     }
 }
